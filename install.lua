6f1162bb45e9e31ed6da941a2fd857516aed7182
SynicalMC <70176240+SynicalMC@users.noreply.github.com>
2022-03-15 14:56:24 -0400

    add easy installation

--- install.lua
+++ install.lua
@@ -6,4 +6,4 @@ local termx, termy = term.getSize()
 local xLoc = (termx / 2) - 2
 local yLoc = (termy / 2) + (termy / 4)
 
-paintutils.drawPixel(xLoc, yLoc)
+paintutils.drawPixel(xLoc, yLoc, colors.red)
