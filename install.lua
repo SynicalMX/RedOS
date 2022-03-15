cc7d6c5cca780d0b437f0d58b7988b03a4677364
SynicalMC <70176240+SynicalMC@users.noreply.github.com>
2022-03-15 16:18:16 -0400

    fixed installation

--- install.lua
+++ install.lua
@@ -1,9 +1,55 @@
-term.setBackgroundColor(colors.white)
+-- Background
+term.setBackgroundColor(colors.gray)
 term.clear()
 
+-- Variable Setup
 local termx, termy = term.getSize()
-
 local xLoc = (termx / 2) - 2
 local yLoc = (termy / 2) + (termy / 4)
 
-paintutils.drawBox(2, yLoc-2,xLoc, termy-1, colors.red)
+-- Cancel Button
+paintutils.drawFilledBox(1, yLoc-1,xLoc-1, termy, colors.lightGray)
+paintutils.drawFilledBox(2, yLoc-2,xLoc, termy-1, colors.red)
+
+-- Install Button
+paintutils.drawFilledBox(termx / 2+1, yLoc-1, termx-1, termy, colors.lightGray)
+paintutils.drawFilledBox(termx-1, yLoc-2, xLoc+4, termy-1, colors.lime)
+
+-- Cancel Text
+xLoc = ((2 + xLoc) / 2) - (string.len("Cancel") / 2)
+yLoc = (termy-2) - ((termy-1) - yLoc-2)
+term.setBackgroundColor(colors.red)
+term.setTextColor(colors.black)
+term.setCursorPos(xLoc, yLoc)
+term.write("Cancel")
+
+-- Install Text
+xLoc = (termx / 2) - 2
+xLoc = (((xLoc+4) + (termx-1)) / 2) - (string.len("Install") / 2)
+term.setBackgroundColor(colors.lime)
+term.setCursorPos(xLoc, yLoc)
+term.write("Install")
+
+-- Credit
+term.setBackgroundColor(colors.gray)
+term.setCursorPos(3,3)
+term.write("RedOS Installer")
+term.setCursorPos(5,3)
+term.write("Made with <3 by SynicalMX")
+
+-- Version
+yLoc = ((termy / 2) - 2)
+xLoc = (termx / 2) - (string.len("RedOS v0.1") / 2)
+term.setCursorPos(xLoc, yLoc)
+term.write("RedOS v0.1")
+
+local mon = peripheral.find("monitor")
+
+-- Loop
+local installing = true
+while installing do
+    local event, button, x, y = os.pullEvent("mouse_click")
+    mon.clear()
+    mon.setCursorPos(1,1)
+    mon.write(x.."    "..y)
+end
